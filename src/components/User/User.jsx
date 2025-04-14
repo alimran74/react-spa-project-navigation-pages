0c60fe4b0f25f1d49fcd82cbf2434f9f72fca2d1
alimran74 <alimranimran748596@gmail.com>
2025-04-14 20:08:18 +0600

    added home button

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,9 +1,13 @@
 import React, { Suspense, useState } from 'react';
-import { Link } from 'react-router';
+// import { Link, Navigate, useLocation } from 'react-router';
 import UserDetails2 from '../UserDetails2/UserDetails2';
 
 const User = ({user}) => {
     const [showInfo, setShowInfo] = useState(false);
+    const [VisitHome, setVisitHome] =useState(false);
+    // const location = useLocation();
+
+
     const {id, name, email, phone} =user;
 
     const userPromise = fetch (`https://jsonplaceholder.typicode.com/users/${id}`)
@@ -14,6 +18,9 @@ const User = ({user}) => {
         borderRadius: '20px',
         padding:'10px',
         margin:'10px'
+    }
+    if(VisitHome){
+        return <Navigate to='/'></Navigate>
     }
     return (
         <div style={userStyle}>
@@ -27,6 +34,7 @@ const User = ({user}) => {
                     <UserDetails2 userPromise={userPromise}></UserDetails2>
                 </Suspense>
             }
+            <button onClick={() => setVisitHome(true)}>Visit Home</button>
         </div>
     );
 };
